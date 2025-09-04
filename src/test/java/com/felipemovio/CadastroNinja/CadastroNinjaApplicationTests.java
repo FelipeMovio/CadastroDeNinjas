740d41d66582b63cc288cc023836d97b137bb85b
FelipeMovio <felipelsmovio@gmail.com>
2025-09-04 20:19:46 -0300

    Inicializando o projeto

---
+++ src/test/java/com/felipemovio/CadastroNinja/CadastroNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipemovio.CadastroNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
