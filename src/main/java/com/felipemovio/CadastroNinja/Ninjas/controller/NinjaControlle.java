17adb7b4688317c9477421cfcf00e4f7dd0de5d2
FelipeMovio <felipelsmovio@gmail.com>
2025-09-08 20:31:03 -0300

    iniciando o controller

--- src/main/java/com/felipemovio/CadastroNinja/Ninjas/controller/NinjaControlle.java
+++ src/main/java/com/felipemovio/CadastroNinja/Ninjas/controller/NinjaControlle.java
@@ -1,10 +1,38 @@
 package com.felipemovio.CadastroNinja.Ninjas.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.felipemovio.CadastroNinja.Ninjas.entitys.Ninja;
+import com.felipemovio.CadastroNinja.Ninjas.services.NinjaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninja")
+@RequiredArgsConstructor
 public class NinjaControlle {
+
+    @Autowired
+    private NinjaService ninjaService;
+
+    // Criar Ninja
+    @PostMapping
+    public void criarNinja(@RequestBody Ninja ninja){
+        ninjaService.cadstraNinja(ninja);
+    }
+
+//    ver todos ninjas criados
+    @GetMapping
+    public ResponseEntity<List<Ninja>> verTodosNinjas(){
+        List<Ninja> ninjas = ninjaService.verNinjas();
+        return ResponseEntity.ok(ninjas);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarNinja(@PathVariable Long id){
+        ninjaService.deletarNinja(id);
+    }
 }
--- src/main/java/com/felipemovio/CadastroNinja/Ninjas/services/NinjaService.java
+++ src/main/java/com/felipemovio/CadastroNinja/Ninjas/services/NinjaService.java
@@ -1,17 +1,22 @@
 package com.felipemovio.CadastroNinja.Ninjas.services;
 
+
 import com.felipemovio.CadastroNinja.Ninjas.entitys.Ninja;
 import com.felipemovio.CadastroNinja.Ninjas.repository.NinjaRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
-@RequiredArgsConstructor
-@Service
-public class NInjaService {
 
+@Service
+public class NinjaService {
+
+    @Autowired
     private NinjaRepository ninjaRepository;
 
     // criar
